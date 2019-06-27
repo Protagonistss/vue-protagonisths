6c4ea775071f199f9a1de56800653197c885d60b
protagonisths <13837050616@163.com>
2019-06-27 16:02:19 +0800

    修复布局问题

--- src/App.vue
+++ src/App.vue
@@ -26,8 +26,11 @@ body {
   min-width: 1366px !important;
   min-height: 668px !important;
   background-color: whitesmoke !important;
+  display: flex;
+  flex-direction: column;
 }
 #app {
+  flex: 1;
   width: 100%;
   height: 100vh;
   display: flex;
